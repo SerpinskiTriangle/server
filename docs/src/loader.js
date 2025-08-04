aa75999ca26c4fc39fcc6253d9830736a44297b1
kol <kolesov.id@gmail.com>
2025-08-04 15:24:50 -0400

    SIM WORKS RAAAGHHHHH

--- docs/src/loader.js
+++ docs/src/loader.js
@@ -1,20 +1,47 @@
 const canvas = document.getElementById("canvas");
 const ctx = canvas.getContext("2d");
 
+function resizeCanvasToContainer(canvas) {
+  const container = canvas.parentNode;
+  const width = container.clientWidth;
+  const height = container.clientHeight;
+
+  canvas.style.width = width + "px";
+  canvas.style.height = height + "px";
+
+  canvas.width = width; //actual resolution
+  canvas.height = height;
+}
+
 Module.onRuntimeInitialized = function() {
-  const initParticles = Module.cwrap("init_particles", "void", ["number", "number"]);
+  const initParticles = Module.cwrap("init_particles", "void", ["number", "number", "number"]);
   const updateParticles = Module.cwrap("update_particles", "void", ["number", "number"]);
   const getParticles = Module.cwrap("get_particles", "number", []);
+  const getLines = Module.cwrap("get_lines", "number", []);
+  const getLineCount = Module.cwrap("get_line_count", "number", []);
+  const generateLines = Module.cwrap("generate_lines", "void", []);
   
-  particle_count = 1;
+  resizeCanvasToContainer(canvas);
 
-  initParticles(100, 100);
+  particle_count = 300;
+
+  const container = canvas.parentNode;
+  const width = container.clientWidth;
+  const height = container.clientHeight;
+
+  initParticles(width, height, particle_count);
 
   function draw(){
-    updateParticles(100, 100);
+    updateParticles(width, height);
+
     const ptr = getParticles();
 
-    ctx.clearRect(0, 0, 100, 100);
+    generateLines();
+
+    const line_ptr = getLines();
+
+    ctx.clearRect(0, 0, width, height);
+
     for (let i = 0; i < particle_count; ++i) {
       const base = ptr / 4 + i * 4;
       const x = Module.HEAPF32[base];
@@ -25,8 +52,24 @@ Module.onRuntimeInitialized = function() {
       ctx.fillStyle = 'white';
       ctx.fill();
     }
+
+    for (let i = 0; i < getLineCount(); i++){
+      const base = line_ptr / 4 + i * 4;
+      const x1 = Module.HEAPF32[base];
+      const y1 = Module.HEAPF32[base + 1];
+      const x2 = Module.HEAPF32[base + 2];
+      const y2 = Module.HEAPF32[base + 3];
+
+      ctx.beginPath();
+      ctx.moveTo(x1, y1);
+      ctx.lineTo(x2, y2);
+      ctx.strokeStyle = 'white';
+      ctx.stroke();
+    }
     requestAnimationFrame(draw);
   }
+
+  
 
   draw();
 };
--- docs/src/sim.c
+++ docs/src/sim.c
@@ -1,5 +1,7 @@
 #include <stdlib.h>
-#define MAX_PARTICLES 1
+#include <math.h>
+
+int max_particles;
 
 struct particle {
   float x;
@@ -8,19 +10,37 @@ struct particle {
   float y_velocity;
 };
 
-struct particle particles[MAX_PARTICLES];
+struct line {
+  float x1;
+  float y1;
+  float x2;
+  float y2;
+};
 
-void init_particles(int window_height, int window_width){
-  for (int i = 0; i < MAX_PARTICLES; i++){
-    particles[i].x = 3;
-    particles[i].y = 3;
-    particles[i].x_velocity = 3;
-    particles[i].y_velocity = 3;
+struct particle *particles;
+
+struct line *lines;
+int line_count;
+
+void init_particles(int window_width, int window_height, int count){
+  max_particles = count;
+
+  particles = (struct particle *)calloc(max_particles, sizeof(struct particle));
+
+  lines = (struct line *)calloc(max_particles * max_particles, sizeof(struct line));// yeah i know
+
+  float speed = 1;
+
+  for (int i = 0; i < max_particles; i++){
+    particles[i].x = rand() % window_width;
+    particles[i].y = rand() % window_height;
+    particles[i].x_velocity = ((float)(rand() % 100) / 100.0f - 0.5f) * speed;
+    particles[i].y_velocity = ((float)(rand() % 100) / 100.0f - 0.5f) * speed;
   }
 }
 
-void update_particles(int window_height, int window_width){
-  for (int i = 0; i < MAX_PARTICLES; i++){
+void update_particles(int window_width, int window_height){
+  for (int i = 0; i < max_particles; i++){
     particles[i].x += particles[i].x_velocity;
     particles[i].y += particles[i].y_velocity;
 
@@ -33,6 +53,46 @@ void update_particles(int window_height, int window_width){
     }
   }
 }
+
+int close(float x1, float y1, float x2, float y2, float threshold){
+  if (pow(pow(x1 - x2, 2) + pow(y1 - y2, 2),0.5) < threshold){
+    return 1;
+  }
+  return 0;
+}
+
+void append_line(float x1, float y1, float x2, float y2){
+  lines[line_count].x1 = x1;
+  lines[line_count].y1 = y1;
+  lines[line_count].x2 = x2;
+  lines[line_count].y2 = y2;
+  line_count++;
+}
+
+void generate_lines(){
+  line_count = 0;
+
+  for(int point_1 = 0; point_1 < max_particles; point_1++){
+    for(int point_2 = 0; point_2 < max_particles; point_2++){
+      if (point_1 == point_2){
+        continue;
+      }
+
+      if (close(particles[point_1].x, particles[point_1].y, particles[point_2].x, particles[point_2].y, 70)){
+        append_line(particles[point_1].x, particles[point_1].y, particles[point_2].x, particles[point_2].y);
+      }
+    }
+  }
+}
+
+int get_line_count(){
+  return line_count;
+}
+
 struct particle *get_particles(){
   return particles;
+}
+
+struct line *get_lines(){
+  return lines;
 }
